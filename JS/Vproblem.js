3b8a9198f3d3ea15eef3fbb8fa051b67ced2a78f
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-12 21:01:35 +0530

    js practicals with vipin varghese question

--- JS/Vproblem.js
+++ JS/Vproblem.js
@@ -1,18 +1,23 @@
 let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 40],[[[70],50]]];
 
-let sum=0
-const findSum=(value)=>{
+
+let sum=0;
+const findOne=(value)=>{
     if(typeof value==='number'){
         sum+=value
-    }else if(typeof value==='object' && value!==null){
+    }
+
+    else if(typeof value=='object' && value!==null){
         for(let key in value){
-            findSum(value[key])
+            findOne(value[key])
         }
+
     }
 }
 
 for(let i=0;i<array.length;i++){
-    findSum(array[i])
+    findOne(array[i])
 }
 
-console.log(sum)
+
+console.log(sum);
