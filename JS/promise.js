7560bff9d8e10ae0e1f83ce341ec72b1cd49bfbf
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-14 22:35:31 +0530

    Promise dadone

--- JS/promise.js
+++ JS/promise.js
@@ -1,3 +1,8 @@
+// 1.Promise basic method
+
+const { response } = require("express");
+
+
 // const promise= new Promise((resolve,reject)=>{
 //     resolve("Success")
 // })
@@ -12,15 +17,51 @@
     
 // })
 
-async function example() {
-    try {
-        const response=await fetch('https://jsonplaceholder.typicode.com/users')
-        const data=await response.json()
-        console.log(data)
-    } catch (error) {
-        console.log(error);
+// async function example() {
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data=await response.json()
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error);
         
-    }
-}
+//     }
+// }
 
-example()
+// example()
+
+//Promise all method
+
+
+const promise1 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("Promise One Success")
+    }, 3000);
+})
+
+const promise2 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("Promise Two Success")
+    }, 2000);
+})
+
+
+const promise3=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("Promise Three Success");
+        // reject("Promise Three Rejected");
+    }, 1000);
+})
+
+
+const promise=Promise.all([promise1,promise2,promise3])
+.then((response)=>{
+    console.log(response);
+    
+})
+.catch((err)=>{
+    console.log('Error',err);
+    
+})
+
+
