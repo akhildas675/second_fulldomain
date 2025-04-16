c1d814d3e1b1e704edad93c12e69a8e95a50464b
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-16 10:47:45 +0530

    Recursion factorial studied

---
+++ DSA/LinkedList/linked_list.js
@@ -0,0 +1,55 @@
+class Node{
+    constructor(value){
+        this.value=value;
+        this.next=null;
+
+    }
+}
+
+
+class linkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.size=0;
+    }
+
+    isEmpty(){
+        return this.size===0
+    }
+
+    append(value){
+        let newNode = new Node(value)
+
+        if(this.isEmpty()){
+            this.head=this.tail=newNode;
+        }else{
+            this.tail.next=newNode
+            this.tail=newNode
+        }
+        this.size++
+    }
+
+    printList(){
+        let curr=this.head;
+        let listValues='';
+        while(curr){
+            listValues+=`${curr.value} -> `;
+            curr=curr.next;
+
+        }
+
+        console.log(listValues);
+        
+    }
+}
+
+
+let list = new linkedList();
+
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
+
+list.printList()
