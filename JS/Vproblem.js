f1020967c44d668c52428a8a042d408804b229af
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-18 18:36:07 +0530

    array of object sum calculation done

--- JS/Vproblem.js
+++ JS/Vproblem.js
@@ -25,21 +25,21 @@ let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 
 // console.log(sum);
 
 
+const obj=[{a:[1,23,2,23]},{a:[1,232,32]},{a:[1,232,32]},{a:[1,232,32]}]
+
 let sum=0;
+
 function findSum(value){
-    if(typeof value==='number'){
-        sum+=value
-    }else if(typeof value==='object' && value!==null){
-        for(let key in value){
-            findSum(value[key])
-        }
+  for(let key in value ){
+    let arr=value[key];
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i]
     }
-    
+  }
 }
 
-
-for(let i=0;i<array.length;i++){
-    findSum(array[i])
+for(let i=0;i<obj.length;i++){
+    findSum(obj[i])
 }
 
-console.log(sum);
+console.log(sum)
