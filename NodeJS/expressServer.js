b4880b048316a821058d409353d55e0a91757704
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-19 12:02:11 +0530

    Express server practiced

---
+++ NodeJS/expressServer.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const path=require('path')
+
+const app=express()
+
+
+const port = process.env.PORT||4000
+
+app.set('view engine','ejs')
+
+app.set('views',path.join(__dirname,'views'))
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+    
+})
