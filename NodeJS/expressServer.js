6a181da18dafdb680c188e2b9c399655feae6259
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-19 22:05:19 +0530

    name parsing and express server done

--- NodeJS/expressServer.js
+++ NodeJS/expressServer.js
@@ -1,21 +1,21 @@
-const express=require('express');
-const path=require('path')
+const express=require('express')
 
+const path=require('path')
 const app=express()
 
 
-const port = process.env.PORT||4000
+const port = process.env.PORT||4500
+
 
 app.set('view engine','ejs')
 
 app.set('views',path.join(__dirname,'views'))
 
-
+const userName='Akhildas H'
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index',{userName})
 })
 
 app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-    
+    console.log(`Server running: http://localhost:${port}`)
 })
