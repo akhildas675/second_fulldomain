ae6f359df9b6d55362e2b2ff5cb6b5421729e2a6
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-29 18:08:02 +0530

    bubblesort with stiring and insertion sort file created

--- DSA/Sorting/bubbleSorting.js
+++ DSA/Sorting/bubbleSorting.js
@@ -21,4 +21,25 @@ let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
 
 // console.log(bubbleSort(array))
 
+// let name='akhildas';
 
+// function bubbleSortName(string){
+//     let swapped;
+//     let str=string.split('')
+//     do {
+//         swapped=false;
+//         for(let i=0;i<str.length-1;i++){
+//             if(str[i]>str[i+1]){
+//                 let temp=str[i];
+//                 str[i]=str[i+1];
+//                 str[i+1]=temp;
+//                 swapped=true
+//             }
+//         }
+//     } while (swapped);
+
+//     return str
+// }
+
+
+// console.log(bubbleSortName(name))
