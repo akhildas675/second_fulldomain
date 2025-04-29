d7229eddddadceca97b693eb31a9575f1104630a
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-29 19:24:16 +0530

    Insertion sort array and string practiced well

--- DSA/Sorting/insertionSort.js
+++ DSA/Sorting/insertionSort.js
@@ -1,1 +1,38 @@
 let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
+
+
+
+// function insertionSort(arr){
+//     for(let i=1;i<arr.length;i++){
+//         let numToInsert=arr[i]
+//         let j=i-1;
+//         while (j>=0 && arr[j]>numToInsert) {
+//             arr[j+1]=arr[j]
+//             j=j-1
+//         }
+//         arr[j+1]=numToInsert
+//     }
+//     return arr
+// }
+
+// console.log('insertion Sort',insertionSort(array));
+
+
+// let name='akhildas'
+
+
+// function insertionSortName(string){
+//     let str=string.split('')
+//     for(let i=1;i<str.length;i++){
+//         let strToInsert=str[i];
+//         j=i-1;
+//         while (j>=0 && str[j]>strToInsert) {
+//             str[j+1]=str[j]
+//             j=j-1
+//         }
+//         str[j+1]=strToInsert
+//     }
+//     return str
+// }
+
+// console.log(insertionSortName(name));
