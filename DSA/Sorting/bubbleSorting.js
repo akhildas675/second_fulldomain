b87a3cff984264d09513899006f2edb5a00ab3d7
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-04-29 18:00:23 +0530

    Bubble sort studied

---
+++ DSA/Sorting/bubbleSorting.js
@@ -0,0 +1,24 @@
+let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
+
+
+// function bubbleSort(arr){
+//     let swapped;
+//     do {
+//         swapped=false;
+//         for(let i=0;i<arr.length;i++){
+//             if(arr[i]>arr[i+1]){
+//                 let temp=arr[i];
+//                 arr[i]=arr[i+1];
+//                 arr[i+1]=temp;
+//                 swapped=true;
+//             }
+//         }
+//     } while (swapped);
+
+//     return arr
+// }
+
+
+// console.log(bubbleSort(array))
+
+
