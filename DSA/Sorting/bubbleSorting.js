6d165c95c7fb80654d2f80cdf746230a891ea219
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-06 08:13:14 +0530

    changes updated

--- DSA/Sorting/bubbleSorting.js
+++ DSA/Sorting/bubbleSorting.js
@@ -21,25 +21,25 @@ let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
 
 // console.log(bubbleSort(array))
 
-// let name='akhildas';
+let name='akhildas';
 
-// function bubbleSortName(string){
-//     let swapped;
-//     let str=string.split('')
-//     do {
-//         swapped=false;
-//         for(let i=0;i<str.length-1;i++){
-//             if(str[i]>str[i+1]){
-//                 let temp=str[i];
-//                 str[i]=str[i+1];
-//                 str[i+1]=temp;
-//                 swapped=true
-//             }
-//         }
-//     } while (swapped);
+function bubbleSortName(string){
+    let swapped;
+    let str=string.split('')
+    do {
+        swapped=false;
+        for(let i=0;i<str.length-1;i++){
+            if(str[i]>str[i+1]){
+                let temp=str[i];
+                str[i]=str[i+1];
+                str[i+1]=temp;
+                swapped=true
+            }
+        }
+    } while (swapped);
 
-//     return str
-// }
+    return str
+}
 
 
-// console.log(bubbleSortName(name))
+console.log(bubbleSortName(name))
