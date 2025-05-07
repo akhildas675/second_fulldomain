7441e70bbf27b98c2f4cc42b86a06efde4f39aab
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-07 18:03:57 +0530

    File changed

--- DSA/Sorting/bubbleSorting.js
+++ DSA/Sorting/bubbleSorting.js
@@ -44,24 +44,3 @@ let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
 
 // console.log(bubbleSortName(name))
 
-
-
-function bubbleSortInArray(arr){
-    let swapped;
-    do{
-        swapped=false;
-        for(let i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                let temp=arr[i];
-                arr[i]=arr[i+1]
-                arr[i+1]=temp;
-                swapped=true
-            }
-        }
-    }while(swapped)
-
-        return arr
-}
-
-
-console.log(bubbleSortInArray(array))
