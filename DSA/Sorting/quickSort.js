d7e9789c1b9dc6a1a782228c9f951ee06dac806d
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-12 20:32:15 +0530

    Quick sort practiced

--- DSA/Sorting/quickSort.js
+++ DSA/Sorting/quickSort.js
@@ -1,27 +1,23 @@
 let array=[4,51,34,25,2,46,234,23,4,42,456,432,23,43,3,32]
 
 function quickSort(arr){
-    if(arr.length<2){
-        return arr
+ if(arr.length<2){
+    return arr
+ }
+ let left=[];
+ let right=[];
+ let pivot=arr[arr.length-1]
+ for(let i=0;i<arr.length-1;i++){
+    if(arr[i]<pivot){
+        left.push(arr[i])
+    }else{
+        right.push(arr[i])
     }
-
-    let left=[];
-    let right=[];
-    let pivot=arr[arr.length-1]
-
-    for(let i=0;i<arr.length-1;i++){
-        if(arr[i]<pivot){
-            left.push(arr[i])
-        }else{
-            right.push(arr[i])
-        }
-    }
-    return [...quickSort(left),pivot,...quickSort(right)]
+ }
+ return [...quickSort(left),pivot,...quickSort(right)]
 }
 
-
-console.log(quickSort(array));
-
+console.log(quickSort(array))
 
 
 let name='akhildas'
