a8483c486a761249524a50bcde1a3353d9e5da03
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-12 14:40:23 +0530

    Call method praticed

--- JS/call_apply_bind.js
+++ JS/call_apply_bind.js
@@ -16,16 +16,30 @@
 
 
 
+// const person={
+//     name:'Akhildas',
+//     id:12,
+// }
+
+
+// function students(...place){
+//     console.log(`My name is ${this.name} I am from ${place}`)
+// }
+
+
+// students.call(person,'Palakkad')
+
+
+
+
 const person={
     name:'Akhildas',
     id:12,
 }
 
 
-function students(...place){
-    console.log(`My name is ${this.name} I am from ${place}`)
+function students(place){
+    console.log(`I am ${this.name}, I am from ${place}`)
 }
 
-
 students.call(person,'Palakkad')
-
