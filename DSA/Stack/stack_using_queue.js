ee131e4eea3884613670cd4d5163d2e8a2d934d4
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-19 10:40:20 +0530

    Stack using Queue has been practiced

--- DSA/Stack/stack_using_queue.js
+++ DSA/Stack/stack_using_queue.js
@@ -5,16 +5,34 @@ class StackUsingQueue{
     }
 
     push(value){
-        this.queue1.push(value);
+        this.queue1.push(value)
     }
 
     pop(){
         if(!this.queue1.length){
-            `The stack is empty`
+            return `The stack is empty`
         }
 
         while(this.queue1.length>1){
-            this.queue2.length(this.queue1.shift())
+            this.queue2.push(this.queue1.shift());
         }
+
+        const popped = this.queue1.shift();
+        [this.queue1.queue2]=[this.queue2,this.queue1];
+        return popped;
+    }
+
+    print(){
+        console.log(this.queue1.join(' -> '));
+        
     }
 }
+
+let stackQueue=new StackUsingQueue()
+stackQueue.push(7)
+stackQueue.push(2)
+stackQueue.push(4)
+stackQueue.push(8)
+stackQueue.push(3)
+stackQueue.print()
+console.log(stackQueue.pop());
