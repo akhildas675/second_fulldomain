714722279b60022bb25c4f4faec0164317f3d1e8
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-20 14:52:17 +0530

    Graph dfs has been practice

---
+++ DSA/Graph/dfsInGraph.js
@@ -0,0 +1,65 @@
+class Graph{
+    constructor(params) {
+        this.adjacencyList={}
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex]=new Set()
+        }
+    }
+
+    addEdge(vertex1,vertex2){
+        if(!this.adjacencyList[vertex1]){
+            this.addEdge(vertex1)
+        }
+
+        if(!this.adjacencyList[vertex2]){
+            this.addEdge(vertex2)
+        }
+
+        this.adjacencyList[vertex1].add(vertex2)
+        this.adjacencyList[vertex2].add(vertex1)
+    }
+
+    dfs(start,visited=new Set()){
+        if(!this.adjacencyList[start]){
+            return;
+        }
+
+        visited.add(start);
+        console.log(start)
+
+        for(let neighbor of this.adjacencyList[start]){
+            if(!visited.has(neighbor)){
+                this.dfs(neighbor,visited)
+            }
+        }
+    }
+
+
+    display(){
+        for(let vertex in this.adjacencyList){
+            console.log(`${vertex} -> ${[...this.adjacencyList[vertex]]}`)
+        }
+    }
+}
+
+let graph = new Graph()
+
+graph.addVertex('a')
+graph.addVertex('b')
+graph.addVertex('c')
+
+
+graph.addEdge('a','b')
+graph.addEdge('a','c')
+graph.addEdge('b','c')
+graph.addEdge('b','a')
+
+graph.display()
+
+graph.dfs("b")
+
+
+
