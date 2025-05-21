dcd3badfcd893db908823d96ab381fb951650685
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-21 21:46:59 +0530

    Binary search tree has been practiced well

---
+++ DSA/Tree/treeBasic.js
@@ -0,0 +1,79 @@
+class Node{
+    constructor(value) {
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class BST{
+    constructor() {
+        this.root=null
+    }
+
+    isEmpty(){
+        return this.root===null;
+    }
+
+    insert(value){
+        const newNode = new Node(value);
+        if(this.isEmpty()){
+            this.root=newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+
+    insertNode(root,node){
+        if(node.value<root.value){
+            if(root.left==null){
+                root.left=node
+            }else{
+                this.insertNode(root.left,node)
+            }
+        }else{
+            if(root.right==null){
+                root.right=node;
+            }else{
+                this.insertNode(root.right,node)
+            }
+        }
+    }
+
+    preOrder(root){
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+    
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value)
+            this.inOrder(root.right)
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value)
+        }
+    }
+}
+
+let bst = new BST()
+
+bst.insert(6)
+bst.insert(5)
+bst.insert(2)
+bst.insert(4)
+bst.insert(1)
+
+// bst.preOrder(bst.root)
+
+// bst.inOrder(bst.root)
+bst.postOrder(bst.root)
