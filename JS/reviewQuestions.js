312d34faf5006831b34093cca43a49eba60a2654
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-28 18:56:05 +0530

    reveiew question paractie

---
+++ JS/reviewQuestions.js
@@ -0,0 +1,28 @@
+// 1. Sum of diagonal matrix
+
+// let matrix = [
+//     [1, 2, 3, 4],
+//     [4, 3, 2, 1],
+//     [6, 5, 4, 3],
+//     [8, 6, 5, 3]
+//   ];
+// function sumOfDiagonal(arr){
+//     let sum1=0;
+
+//     for(let i=0;i<arr.length;i++){
+//         sum1+=arr[i][i]+arr[i][arr.length-i-1]
+//     }
+//    return sum1
+// }
+
+// console.log(sumOfDiagonal(matrix))
+
+
+//2. Spiral Order Matrix Traversal Input: [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ]  
+//   Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+
