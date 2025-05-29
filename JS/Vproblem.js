683d5fff57ac31d200a44b3c73aa6324f09a62e7
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-05-29 17:36:38 +0530

    V probelm practiced

--- JS/Vproblem.js
+++ JS/Vproblem.js
@@ -25,21 +25,75 @@ let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 
 // console.log(sum);
 
 
-const obj=[{a:[1,23,2,23]},{a:[1,232,32]},{a:[1,232,32]},{a:[1,232,32]}]
+// const obj=[{a:[1,23,2,23]},{a:[1,232,32]},{a:[1,232,32]},{a:[1,232,32]}]
 
-let sum=0;
+// let sum=0;
 
-function findSum(value){
-  for(let key in value ){
-    let arr=value[key];
-    for(let i=0;i<arr.length;i++){
-        sum+=arr[i]
-    }
-  }
-}
+// function findSum(value){
+//   for(let key in value ){
+//     let arr=value[key];
+//     for(let i=0;i<arr.length;i++){
+//         sum+=arr[i]
+//     }
+//   }
+// }
 
-for(let i=0;i<obj.length;i++){
-    findSum(obj[i])
-}
+// for(let i=0;i<obj.length;i++){
+//     findSum(obj[i])
+// }
 
-console.log(sum)
+// console.log(sum)
+
+
+
+
+//Practice
+
+
+// let sum=0;
+
+// const findTheSum=(values)=>{
+//     if(typeof values=='number'){
+//       sum+=values
+//     }else if(typeof values ==='object' && values!==null && values!==undefined){
+//       for(let key in values){
+//         findTheSum(values[key])
+//       }
+//     }
+// }
+
+
+
+
+
+// for(let i=0;i<array.length;i++){
+//   console.log(array[i])
+// }
+
+// console.log(sum)
+
+
+
+
+// let sum=0
+// function findTheSum(value){
+//   if(typeof value=='number'){
+//     sum+=value
+//   }else if(Array.isArray(value)){
+
+//     for(let i=0;i<value.length;i++){
+//       findTheSum(value[i])
+//     }
+//   }else if(typeof value==='object' && value!==null && value!==undefined){
+//     for(let key in value){
+//       findTheSum(value[key])
+//     }
+//   }
+
+// }
+
+// for(let i=0;i<array.length;i++){
+//   findTheSum(array[i])
+// }
+
+// console.log(sum)
