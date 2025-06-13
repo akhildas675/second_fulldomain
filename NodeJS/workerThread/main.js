5fa395bad7d2d6d0ecfc2272bc246e59d80b968e
AKHILDAS - H <akhildas675@gmail.com>
2025-06-13 22:16:48 +0530

    .env updated

--- NodeJS/workerThread/main.js
+++ NodeJS/workerThread/main.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const Worker= require('worker_threads')
+const {Worker}= require('worker_threads')
 
 const worker=new Worker(path.resolve(__dirname,'worker.js'))
 
@@ -9,3 +9,5 @@ worker.on('message',(result)=>{
     console.log('The result from worker ',result);
     
 })
+
+console.log('Without blocking main thread')
