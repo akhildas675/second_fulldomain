b8ba473f95810c7ff9e68853cea5c5058c98a7d4
AKHILDAS - H <akhildas675@gmail.com>
2025-06-15 20:16:53 +0530

    Worker thread practiced well

--- NodeJS/workerThread/main.js
+++ NodeJS/workerThread/main.js
@@ -1,13 +1,25 @@
+// const path = require('path')
+// const {Worker}= require('worker_threads')
+
+// const worker=new Worker(path.resolve(__dirname,'worker.js'))
+
+// worker.postMessage(5)
+
+// worker.on('message',(result)=>{
+//     console.log('The result from worker ',result);
+    
+// })
+
+// console.log('Without blocking main thread')
+
 const path = require('path')
-const {Worker}= require('worker_threads')
+const {Worker}=require('worker_threads')
 
 const worker=new Worker(path.resolve(__dirname,'worker.js'))
 
 worker.postMessage(5)
 
 worker.on('message',(result)=>{
-    console.log('The result from worker ',result);
+    console.log(`The result from worker thread is : ${result}`);
     
 })
-
-console.log('Without blocking main thread')
--- NodeJS/workerThread/worker.js
+++ NodeJS/workerThread/worker.js
@@ -1,6 +1,12 @@
-const {parentPort}=require('worker_threads')
+// const {parentPort}=require('worker_threads')
 
+// parentPort.on('message',(value)=>{
+//     const result=value*2
+//     parentPort.postMessage(result)
+// })
+
+const {parentPort}=require('worker_threads')
 parentPort.on('message',(value)=>{
-    const result=value*2
+    let result=value*2
     parentPort.postMessage(result)
 })
