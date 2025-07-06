3b3342e2bcdd3222f58a4f539ffa84141f567c61
AKHILDAS - H <akhildas675@gmail.com>
2025-07-06 21:15:12 +0530

    queue using stack code mistaken corrected

--- DSA/Queue/queue_using_stack.js
+++ DSA/Queue/queue_using_stack.js
@@ -4,36 +4,28 @@ class QueueUsingStack {
     this.stack2 = [];
   }
 
-  enqueue(value){
-    this.stack1.push(value)
+  enqueue(value) {
+    this.stack1.push(value); // Push into input stack
   }
 
-  dequeue(){
-    if(!this.stack1.length && !this.stack2.length){
-        return `The queue is empty`
+  dequeue() {
+    if (!this.stack1.length && !this.stack2.length) {
+      return "The queue is empty";
     }
-    if(!this.stack2.length){
-        while(this.stack1.length){
-            this.stack2.push(this.stack1.shift())
-        }
+
+    if (!this.stack2.length) {
+      while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop()); // use pop instead of shift
+      }
     }
-    return this.stack2.pop()
+
+    return this.stack2.pop(); // Dequeue from output stack
   }
-  print(){
-    console.log([...this.stack2.reverse(),...this.stack1].join("<-"));
-    
+
+  print() {
+    // Create safe copies for display
+    const output = [...this.stack2].reverse(); // Reverse output stack
+    const input = [...this.stack1];            // Input stack (as is)
+    console.log([...output, ...input].join(" <- "));
   }
 }
-
-
-let queueStack = new QueueUsingStack()
-
-queueStack.enqueue(5)
-queueStack.enqueue(3)
-queueStack.enqueue(6)
-queueStack.enqueue(2)
-queueStack.enqueue(1)
-
-console.log("Dequeued:",queueStack.dequeue());
-
-queueStack.print()
