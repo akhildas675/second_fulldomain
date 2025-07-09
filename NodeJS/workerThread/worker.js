001602e892cdc223d93093e3cb3afb220333d33b
AKHILDAS - H <akhildas675@gmail.com>
2025-07-09 12:45:33 +0530

    practiced worker thread and changes made in gitignore file

--- NodeJS/workerThread/worker.js
+++ NodeJS/workerThread/worker.js
@@ -1,4 +1,12 @@
-const {parentPort}= require('worker_threads')
+// const {parentPort}= require('worker_threads')
+
+// parentPort.on('message',(value)=>{
+//     let result=value*10
+//     parentPort.postMessage(result)
+// })
+
+
+const {parentPort} =require('worker_threads')
 
 parentPort.on('message',(value)=>{
     let result=value*10
