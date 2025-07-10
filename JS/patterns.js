f26edb385e539e4a4d91e5f81fef4fa27824d7cd
AKHILDAS - H <akhildas675@gmail.com>
2025-07-10 19:53:04 +0530

    pattern checked

--- JS/patterns.js
+++ JS/patterns.js
@@ -100,16 +100,16 @@
 
 // NumberPattern(5)
 
-let val=[1,2,3,4,5]
-let result=[]
-for(let i=0;i<val.length;i++){
-    let obj={}
+// let val=[1,2,3,4,5]
+// let result=[]
+// for(let i=0;i<val.length;i++){
+//     let obj={}
     
-    for(let j=0;j<=i;j++){
-        let key=`a${j+1}`
-        obj[key]=val[j]
+//     for(let j=0;j<=i;j++){
+//         let key=`a${j+1}`
+//         obj[key]=val[j]
        
-    }
-        result.push(obj)
-}
-console.log(result);
+//     }
+//         result.push(obj)
+// }
+// console.log(result);
