482bf3475e76df5931bae69ab69d91655eef803c
AKHILDAS - H <akhildas675@gmail.com>
2025-09-21 17:07:32 +0530

    worker thread practice

--- NodeJS/workerThread/main.js
+++ NodeJS/workerThread/main.js
@@ -1,20 +1,22 @@
-const path = require('path');
-const {Worker}=require('worker_threads');
+const path = require('path')
+const {Worker}=require('worker_threads')
 
 
-const worker = new Worker(path.resolve(__dirname,'worker.js'))
-
+const worker = new Worker(path.resolve(__dirname,'worker.js'));
 
 worker.postMessage(10)
 
-worker.on('message',(result)=>{
-    console.log(`The result is: ${result}`)
+
+worker.on('message',(msg)=>{
+    console.log(msg)
 })
+
 
 worker.on('error',(err)=>{
-    console.error(err)
+    console.log(err)
 })
 
+
 worker.on('exit',(code)=>{
-    console.log('Code executed',code)
+    console.log('code executed',code)
 })
