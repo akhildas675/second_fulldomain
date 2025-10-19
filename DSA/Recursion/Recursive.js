426561f263811b1255a1146c9b8f855231ca4248
akhildas675 <akhildas675@gmail.com>
2025-10-19 09:18:22 +0530

    Refactor: Comment out recursive functions for clarity

--- DSA/Recursion/Recursive.js
+++ DSA/Recursion/Recursive.js
@@ -1,14 +1,65 @@
 //Recursion
 
 
+/*
+1.Print Number
+2.
 
-function printNum(n){
-    if(n>=10){
-        return n
-    }
+*/
 
-    console.log(n);
-    return printNum(n+1)
-}
 
-console.log(printNum(1))
+//1.Print Number
+
+// function printNum(n){
+//     if(n>=10){
+//         return n
+//     }
+
+//     console.log(n);
+//     return printNum(n+1)
+// }
+
+// console.log(printNum(1))
+
+
+
+
+//2.Print Reverse
+
+
+
+// function printReverse(n){
+
+//     if(n==0){
+//         return n
+//     }
+
+//     console.log(n)
+//     return printReverse(n-1)
+
+// }
+
+// console.log(printReverse(10))
+
+
+
+
+//3.Sum of first 10
+
+
+// function sumOfFirst(n){
+
+//     if(n==10){
+//         return n
+//     }
+ 
+//     return n+sumOfFirst(n+1)
+
+// }
+
+// console.log(sumOfFirst(1))
+
+
+
+
+//4 
