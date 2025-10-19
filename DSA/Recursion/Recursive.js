06615550d5baf5a01e34e0915395f3a58e1f7046
akhildas675 <akhildas675@gmail.com>
2025-10-19 16:28:42 +0530

    Refactor: Update comments and clean up code structure for clarity

--- DSA/Recursion/Recursive.js
+++ DSA/Recursion/Recursive.js
@@ -1,12 +1,13 @@
 //Recursion
 
-
 /*
 1.Print Number
-2.
+2.Print Reverse
+3.Sum of first 10
+4.First 10 Factorial Numbers
+
 
 */
-
 
 //1.Print Number
 
@@ -22,11 +23,9 @@
 // console.log(printNum(1))
 
 
-
+//---------------------------------------------------
 
 //2.Print Reverse
-
-
 
 // function printReverse(n){
 
@@ -41,31 +40,44 @@
 
 // console.log(printReverse(10))
 
-
-
+//--------------------------------------------------
 
 //3.Sum of first 10
-
 
 // function sumOfFirst(n){
 
 //     if(n==10){
 //         return n
 //     }
- 
+
 //     return n+sumOfFirst(n+1)
 
 // }
 
 // console.log(sumOfFirst(1))
 
-
-
+//------------------------------------------------
 
 // Factorial
 
-
 //4.First 10 Factorial Numbers
+
+// function fac(n) {
+//   if (n == 0) {
+//     return 1;
+//   }
+
+//   return n * fac(n - 1);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(fac(i));
+// }
+
+
+//--------------------------------------------------------
+
+//5.print first 10 factorial without loop;
 
 
 
@@ -78,7 +90,16 @@ function fac(n){
 }
 
 
+function printNumber(n,limit){
 
-for(let i=0;i<=10;i++){
-    console.log(fac(i))
+    if(n==limit){
+        return;
+    }
+
+    console.log(fac(n))
+
+    return printNumber(n+1,limit)
+
 }
+
+printNumber(1,10)
