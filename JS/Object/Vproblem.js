c3cf681242ca3a977d9917c6debe523089d33204
akhildas675 <akhildas675@gmail.com>
2025-10-28 07:50:32 +0530

    Practice review questions

--- JS/Object/Vproblem.js
+++ JS/Object/Vproblem.js
@@ -1,4 +1,4 @@
-let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 40],[[[70],50]]];
+// let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 40],[[[70],50]]];
 
 //Easy method
 
@@ -102,16 +102,55 @@ let array = [23, { a: 20, b: { c: 52 } }, [[10, 50]], undefined, null, [10, 50, 
 
 
 
-let obj={a:1,b:3,c:4,d:4,f:4,f:'3'}
+// let obj={a:1,b:3,c:4,d:4,f:4,f:'3'}
 
-let seen={}
-for(let key in obj){
-    let num=obj[key]
-    if(!seen[num]){
-        seen[num]=1
-    }else{
-        seen[num]++
+// let seen={}
+// for(let key in obj){
+//     let num=obj[key]
+//     if(!seen[num]){
+//         seen[num]=1
+//     }else{
+//         seen[num]++
+//     }
+// }
+
+// console.log(seen)
+
+// Given an array of positive integers, find and print all the perfect numbers in the array.
+
+// A perfect number is a positive integer that is equal 
+// to the sum of its proper divisors (excluding itself).
+
+// Example:
+
+// 6 → 1 + 2 + 3 = 6 (Perfect)
+// 28 → 1 + 2 + 4 + 7 + 14 = 28 (Perfect)
+// Example 1
+
+// Output:6, 28
+
+
+// Input:
+
+let array = [6, 28, 12, 97,27,496,8128,2,1]
+
+
+
+
+function perfectNumber(arr){
+    let result=[]
+    for(let i=0;i<arr.length;i++){
+        let sum=0
+        for(let j=1;j<arr[i];j++){
+            if(j<arr[i] && arr[i]%j==0){
+                sum+=j
+            }
+        }
+        if(sum==arr[i]){
+            result.push(arr[i])
+        }
     }
+    return result
 }
 
-console.log(seen)
+console.log(perfectNumber(array))
